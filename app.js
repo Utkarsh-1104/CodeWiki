1ada2c6d24b915e67d6f97c3148af4051747f13c
lonewolf <bajpaiutkarsh1104@gmail.com>
2024-02-01 16:11:52 +0530

    Add dotenv configuration and connect to MongoDB

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
+const URI = process.env.MONGODB_URI
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost:27017/wikiDB')
+async function main() {
 
-const articleSchema = {
-    title: String,
-    content: String
+    mongoose.connect(URI)
+    
+    const articleSchema = {
+        title: String,
+        content: String
+    }
+    
+    const Article = mongoose.model("Article", articleSchema)
+
+    app.get('/', (req, res) => { 
+        res.send('<h1>Hello World</h1>')
+    })
+    
+    app.get('/articles', async (req, res) => {
+        const allArticles = await Article.find()
+        console.log(allArticles);
+    })
+   
 }
-
-const Article = mongoose.model("Article", articleSchema)
-
-app.get('/', (req, res) => { 
-    res.send('<h1>Hello World</h1>')
-})
-
-app.get('/articles', async (req, res) => {
-    const allArticles = await Article.find()
-    console.log(allArticles);
-})
-
 app.listen(2000, () => {
     console.log('sever running on http://localhost:2000');
 })
+main()
